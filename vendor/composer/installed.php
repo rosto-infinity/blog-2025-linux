cf6ea0a61de48bf1e4b83e76a5c4b607ae6ab54f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-12 23:36:49 +0100

    fdf

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '95847352e0a2fb1d872937d20e3c05aac8771125',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '95847352e0a2fb1d872937d20e3c05aac8771125',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jasongrimes/paginator' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => '3411e3cd0c6479a0b514f26e4358f0273552f221',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jasongrimes/paginator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
